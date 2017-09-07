4b184d67078666892b4470084be21aa158d22013
zhenghao <zjz_zh5319@163.com>
2017-09-07 09:33:16 +0800

    [Mr.zheng]：angular2新建组件、指令、服务

--- angular2-demo/src/app/app.component.ts
+++ angular2-demo/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
+import { LogServiceService } from './log-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+   private title : string;
+   private isShowMore : boolean;
+
+   constructor(private logService : LogServiceService){
+
+   }
+
+   ngOnInit(){
+      this.title = 'Angular 2';
+      this.logService.debug("根组件被初始化...");
+   }
 }
--- angular2-demo/src/app/app.module.ts
+++ angular2-demo/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { LogServiceService } from './log-service.service';
+
+import { HelloComponent } from './hello/hello.component';
+import { MyFirstDirectiveDirective } from './my-first-directive.directive';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    MyFirstDirectiveDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [LogServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
